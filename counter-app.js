e8ed8e59bb31eb4365a47ed9d2f90fb213f5faa9
Brandon Park <bdp5295@psu.edu>
2026-02-20 17:11:08 -0500

    Styling for count-label and comments added

--- counter-app.js
+++ counter-app.js
@@ -34,8 +34,8 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     });
 
     this.count = 0;
-    this.min = 5;
-    this.max = 25;
+    this.min = -50;
+    this.max = 50;
   }
 
   // Lit reactive properties
@@ -65,11 +65,27 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
       :host([count="21"]) h3 {
         color: var(--ddd-theme-default-accent);
       }
+      .count-label.bound {
+        color: var(--ddd-theme-default-original87Pink);
+      }
       .count-label {
-        font: 70px var(--ddd-font-primary);
+        font: var(--ddd-font-size-3xl) var(--ddd-font-secondary);
+        margin: var(--ddd-spacing-4);
       }
       button {
-        margin: var(--ddd-spacing-3)
+        margin: var(--ddd-spacing-2)
+      }
+      .decrement-button:hover {
+        background-color: var(--ddd-theme-default-discoveryCoral);
+      }
+      .decrement-button:focus {
+        background-color: var(--ddd-theme-default-pughBlue);
+      }
+      .increment-button:hover {
+        background-color: var(--ddd-theme-default-discoveryCoral);
+      }
+      .increment-button:focus {
+        background-color: var(--ddd-theme-default-pughBlue);
       }
       .wrapper {
         margin: var(--ddd-spacing-2);
@@ -77,6 +93,7 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
       }
       h3 span {
         font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+        margin: var(--ddd-spacing-12);
       }
     `];
   }
@@ -86,14 +103,21 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     return html`
 <div class="wrapper">
   <confetti-container id="confetti">
-    <h3 class="count-label">${this.count}</h3>
-    <button @click="${this.decrement}" ?disabled="${this.min === this.count}">-</button>
-    <button @click="${this.increment}" ?disabled="${this.max === this.count}">+</button>
+  <!--The class will add bound to the end to change the color of count-label when count is at the min or max-->
+    <h3 class="count-label ${this.count === this.min || this.count === this.max ? "bound" : ""}">
+      ${this.count}
+    </h3>
+    <button class="decrement-button" @click="${this.decrement}" ?disabled="${this.min === this.count}">-</button>
+    <button class="increment-button" @click="${this.increment}" ?disabled="${this.max === this.count}">+</button>
     <slot></slot>
   </confetti-container> 
 </div>`;
   }
 
+  /**
+   * increases the count by 1 until count reaches the max value
+   * once count reaches the max value, the count stops increasing and returns
+   */
   increment() {
     if (this.count == this.max) {
       return;
@@ -101,6 +125,10 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     this.count++;
   }
 
+  /**
+   * decreases the count by 1 until count reaches the min value
+   * once count reaches the min value, the count stops decreasing and returns
+   */
   decrement() {
     if (this.count == this.min) {
       return;
@@ -117,37 +145,37 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   }
 
   
-updated(changedProperties) {
-  if (super.updated) {
-    super.updated(changedProperties);
-  }
-  if (changedProperties.has('count')) {
-    // do your testing of the value and make it rain by calling makeItRain
-    if (this.count === 21) {
-      this.makeItRain();
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    if (changedProperties.has('count')) {
+      // do your testing of the value and make it rain by calling makeItRain
+      if (this.count === 21) {
+        this.makeItRain();
+      }
     }
   }
-}
 
-makeItRain() {
-  // this is called a dynamic import. It means it won't import the code for confetti until this method is called
-  // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
-  // will only run AFTER the code is imported and available to us
-  import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
-    (module) => {
-      // This is a minor timing 'hack'. We know the code library above will import prior to this running
-      // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
-      // this "hack" ensures the element has had time to process in the DOM so that when we set popped
-      // it's listening for changes so it can react
-      setTimeout(() => {
-        // forcibly set the poppped attribute on something with id confetti
-        // while I've said in general NOT to do this, the confetti container element will reset this
-        // after the animation runs so it's a simple way to generate the effect over and over again
-        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
-      }, 0);
-    }
-  );
-}
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
 }
 
 globalThis.customElements.define(CounterApp.tag, CounterApp);
